04358efbf22e8eef7c5c1257bc4daa3524a056b4
DegeneratorDi <D.Dovgulevich@ya.ru>
2016-11-28 00:12:15 +0300

    допиливаем джойстик

--- src/Joystick.java
+++ src/Joystick.java
@@ -1,7 +1,63 @@
-package PACKAGE_NAME;
+import purejavahidapi.*;
 
-/**
- * Created by Digger on 27.11.2016.
- */
-public class joystick {
+import java.io.IOException;
+import java.util.List;
+
+public class Joystick {
+
+    private List<HidDeviceInfo> devList;
+    private HidDevice devise;
+    private byte[] devOut;
+
+    Joystick(){
+        devList = PureJavaHidApi.enumerateDevices();
+        devOut=new byte[8];
+    }
+
+    void getHidDevices() {
+
+
+        for (HidDeviceInfo info : devList) {
+            System.out.printf("VID = 0x%04X PID = 0x%04X Manufacturer = %s Product = %s Path = %s\n", //
+                    info.getVendorId(), //
+                    info.getProductId(), //
+                    info.getManufacturerString(), //
+                    info.getProductString(), //
+                    info.getPath());
+        }
+    }
+
+    void setDevise(int num) throws IOException {
+
+        devise = PureJavaHidApi.openDevice(devList.get(num));
+    }
+
+    void run(){
+        devise.setInputReportListener(new InputReportListener() {
+            @Override
+            public void onInputReport(HidDevice source, byte Id, byte[] data, int len) {
+                devOut = data;
+            }
+        });
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    void test(int num) {
+        for (int j = 0; j < num; j++) {
+            for (int i = 0; i < 8; i++)
+                System.out.printf("%02d ", devOut[i]);
+                System.out.println();
+
+
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Robot robot = new Robot();
-        String hostname = "127.0.0.1";
+        Joystick joystick = new Joystick();
 
-        robot.connect(hostname,true);
+        //String hostname = "127.0.0.1";
+        //robot.connect(hostname,true);
+        //float s =1f;
+       // robot.move(s, s, s);
 
-        float s =0.6f;
-
-            robot.move(s, s, s);
-
-
+        joystick.getHidDevices();
+        joystick.setDevise(0);
+        joystick.run();
+        joystick.test(10);
 
 
 
