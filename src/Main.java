92c39d37c41d26d609654058c8ff2713abbef47a
DegeneratorDi <D.Dovgulevich@ya.ru>
2016-12-01 16:04:24 +0300

    прикручиваю камеру

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,43 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
+
         Robot robot = new Robot();
         Joystick joystick = new Joystick();
 
-
-        String hostname = "127.0.0.1";
+        String hostname = "172.26.1.1";
+        //String hostname = "127.0.0.1";
         robot.connect(hostname,true);
         float s =1f, rs=-1f;
        robot.move(s, s, s);
 
         joystick.getHidDevices();
-        joystick.setDevise(0);
+        joystick.setDevise(1);
         joystick.run();
         joystick.test(1);
 
+
+        MainFrame frame = new MainFrame(robot);
+        frame.setTitle( "Robotino  GUI" );
+
+
         while(true) {
+
+            if(joystick.getStatusButton("1")){
+                s=0.1f; rs=-0.1f;
+            }
+
+            if(joystick.getStatusButton("2")){
+                s=0.25f; rs=-0.25f;
+            }
+
+            if(joystick.getStatusButton("3")){
+                s=0.50f; rs=-0.50f;
+            }
+
+            if(joystick.getStatusButton("4")){
+                s=1f; rs=-1f;
+            }
 
             if(joystick.getStatusButton("up")) {
                 robot.move(s, 0, 0);
--- src/Robot.java
+++ src/Robot.java
@@ -1,20 +1,30 @@
 import rec.robotino.api2.Com;
 import rec.robotino.api2.OmniDrive;
+import rec.robotino.api2.Camera;
 
+import java.awt.*;
+import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class Robot {
 
-
+    private final Camera _camera;
     private final Com _com;
     private final OmniDrive _omniDrive;
 
+    protected final Collection<RobotListener> _listeners;
+
    public Robot() {
+       _camera = new MyCamera();
        _com = new Com();
        _omniDrive = new OmniDrive();
 
        _omniDrive.setComId(_com.id());
-    }
+        _camera.setComId(_com.id());
+
+       _listeners = new CopyOnWriteArrayList<RobotListener>();
+   }
 
    public void connect(String hostname, boolean block){
 
@@ -26,12 +36,38 @@ public class Robot {
     public void move(float x, float y, float h) {
 
        _omniDrive.setVelocity(x,y,h);
+
        try {
             Thread.sleep(10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         _omniDrive.setVelocity(0,0,0);
+    }
+
+    class MyCamera extends Camera
+    {
+        public MyCamera()
+        {
+            setCameraNumber(0);
+            setBGREnabled(true);
+        }
+
+        @Override
+        public void imageReceivedEvent(Image img, long dataSize, long width, long height, long step)
+        {
+            for(RobotListener listener : _listeners) {
+                listener.onImageReceived(img);
+            }
+        }
+    }
+
+
+
+
+    public void addListener(RobotListener listener)
+    {
+        _listeners.add( listener );
     }
 
 
